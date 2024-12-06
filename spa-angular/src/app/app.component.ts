a34dfc7dec51b623e972b892258b10dff73ca653
Jovan-Y <yometmank@gmail.com>
2024-12-06 11:15:24 +0700

    sdasda
    sdasda

--- spa-angular/src/app/app.component.ts
+++ spa-angular/src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterModule, RouterOutlet } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
   imports: [RouterOutlet, RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
