ab70a0ee3df4712198cda29a153d99a4c7f9673e
jovan <yometmank@gmail.com>
2024-12-20 10:45:47 +0700

    asd
    asd

--- src/app/auth.guard.spec.ts
+++ src/app/auth.guard.spec.ts
@@ -1,28 +1,17 @@
 import { TestBed } from '@angular/core/testing';
-import { Router } from '@angular/router';
-import { AuthGuard } from './auth.guard';
+import { CanActivateFn } from '@angular/router';
 
-describe('AuthGuard', () => {
-  let guard: AuthGuard;
-  let routerSpy: jasmine.SpyObj<Router>;
+import { authGuard } from './auth.guard';
+
+describe('authGuard', () => {
+  const executeGuard: CanActivateFn = (...guardParameters) => 
+      TestBed.runInInjectionContext(() => authGuard(...guardParameters));
 
   beforeEach(() => {
-    const spy = jasmine.createSpyObj('Router', ['navigate']);
-    TestBed.configureTestingModule({
-      providers: [AuthGuard, { provide: Router, useValue: spy }],
-    });
-    guard = TestBed.inject(AuthGuard);
-    routerSpy = TestBed.inject(Router) as jasmine.SpyObj<Router>;
+    TestBed.configureTestingModule({});
   });
 
-  it('should allow navigation if token exists', () => {
-    spyOn(localStorage, 'getItem').and.returnValue('mockToken');
-    expect(guard.canActivate()).toBeTrue();
-  });
-
-  it('should redirect to /auth if token does not exist', () => {
-    spyOn(localStorage, 'getItem').and.returnValue(null);
-    expect(guard.canActivate()).toBeFalse();
-    expect(routerSpy.navigate).toHaveBeenCalledWith(['/auth']);
+  it('should be created', () => {
+    expect(executeGuard).toBeTruthy();
   });
 });
